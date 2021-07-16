eb3b7b634debf640220408fef83e3c732991186a
rsi2m <sergei@plumbr.eu>
2021-07-16 09:55:52 +0300

    progress

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 A new Flutter project.
 
-065
+073
 
 ## Getting Started
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,82 +1,40 @@
 import 'package:flutter/material.dart';
 
-import 'quiz.dart';
-import 'result.dart';
-
 void main() {
-  runApp(MyApp());
+  runApp(ExpenseApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  var _questionIndex = 0;
-  var _totalScore = 0;
-
-  void restartQuiz() {
-    setState(() {
-      _questionIndex = 0;
-      _totalScore = 0;
-    });
-  }
-
-  void _answerQuestion(int score) {
-    _totalScore += score;
-
-    setState(() {
-      _questionIndex = _questionIndex + 1;
-    });
-    print(_questionIndex);
-    if (_questionIndex < _questions.length) {
-      print("We have more questions!");
-    }
-  }
-
-  final _questions = const [
-    {
-      'text': 'Какой цвет?',
-      'answers': [
-        {'text': 'Черный', 'score': 10},
-        {'text': 'Белый', 'score': 5},
-        {'text': 'Зелёный', 'score': 3},
-        {'text': 'Желтый', 'score': 2},
-      ]
-    },
-    {
-      'text': 'Какое животное?',
-      'answers': [
-        {'text': 'Кот', 'score': 10},
-        {'text': 'Пес', 'score': 5},
-        {'text': 'Гусь', 'score': 3},
-        {'text': 'Червь', 'score': 0},
-      ]
-    },
-    {
-      'text': 'Какой город?',
-      'answers': [
-        {'text': 'СФ', 'score': 10},
-        {'text': 'Прага', 'score': 5},
-        {'text': 'Ору', 'score': 3},
-        {'text': 'Сомпа', 'score': 0}
-      ]
-    }
-  ];
-
+class ExpenseApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Дарова, Серёга!"),
-        ),
-        body: _questionIndex < _questions.length
-            ? Quiz(_questionIndex, _questions, _answerQuestion)
-            : Result(_totalScore, restartQuiz),
+      title: "Expenses App",
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter App"),
+      ),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text("Chart"),
+              elevation: 5,
+            ),
+          ),
+          Card(
+            child: Text("List of TX"),
+          )
+        ],
       ),
     );
   }
